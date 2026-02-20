d5a49e2994e183a2dd171e4be25e72825cc998d3
WatIsDis31 <vincentiusedward31@gmail.com>
2026-02-20 18:14:33 +0700

    update: updated auth router, added github callback for github oauth

--- routes/auth.js
+++ routes/auth.js
@@ -7,4 +7,17 @@ router.post("/login", AuthController.login);
 router.post("/google-login", AuthController.googleLogin);
 router.post("/github-login", AuthController.githubLogin);
 
+router.get("/github/callback", (req, res) => {
+  const { code } = req.query;
+
+  if (!code) return res.status(400).send("No code received from GitHub");
+
+  res.send(`
+    <script>
+      window.opener.postMessage({ githubCode: "${code}" }, "*");
+      window.close();
+    </script>
+  `);
+});
+
 module.exports = router;
